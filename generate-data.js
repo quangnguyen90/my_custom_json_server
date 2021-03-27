56e020c412e85bb71f312b21322ee0f25376a6b6
quangnguyen90 <nguyenphuquang90@gmail.com>
2021-03-27 14:28:59 +0700

    5- Generate & Log random data for Product by Category

--- generate-data.js
+++ generate-data.js
@@ -1,41 +1,64 @@
-const faker = require('faker');
-const fs = require('fs');
+const faker = require("faker");
+const fs = require("fs");
 
 // Set locale to use Vietnamese
-faker.locale = 'vi';
+faker.locale = "vi";
 
 // Random data
 const randomCategoryList = (n) => {
-    if (n <= 0) return [];
-    const categoryList = [];
-    Array.from(new Array(n)).forEach(() => {
-        const category = {
-            id: faker.random.uuid(),
-            name: faker.commerce.department(),
-            createdAt: Date.now(),
-            updatedAt: Date.now()
-        };
-        categoryList.push(category);
+  if (n <= 0) return [];
+  const categoryList = [];
+  Array.from(new Array(n)).forEach(() => {
+    const category = {
+      id: faker.random.uuid(),
+      name: faker.commerce.department(),
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    categoryList.push(category);
+  });
+  return categoryList;
+};
+
+const randomProductList = (categoryList, numberOfProducts) => {
+  if (numberOfProducts <= 0) return [];
+  const productList = [];
+  for (const category of categoryList) {
+    Array.from(new Array(numberOfProducts)).forEach(() => {
+      const product = {
+        categoryId: category.id,
+        id: faker.random.uuid(),
+        name: faker.commerce.productName(),
+        color: faker.commerce.color(),
+        price: faker.commerce.price(),
+        description: faker.commerce.productDescription(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        thumbnailUrl: faker.image.imageUrl(400, 400),
+      };
+      productList.push(product);
     });
-    return categoryList;
+  }
+  return productList;
 };
 
 // IFFE
 (() => {
-    // random data
-    const categoryList = randomCategoryList(4);
+  // random data
+  const categoryList = randomCategoryList(4);
+  const productList = randomProductList(categoryList, 5);
 
-    // prepare db object
-    const db = {
-        categories: categoryList,
-        products: [],
-        profile: {
-            name: "Po",
-        }
-    };
+  // prepare db object
+  const db = {
+    categories: categoryList,
+    products: productList,
+    profile: {
+      name: "Po",
+    },
+  };
 
-    // write db object to db.json
-    fs.writeFile('db.json', JSON.stringify(db), () => {
-        console.log("Generated data successfully");
-    });
+  // write db object to db.json
+  fs.writeFile("db.json", JSON.stringify(db), () => {
+    console.log("Generated data successfully");
+  });
 })();
