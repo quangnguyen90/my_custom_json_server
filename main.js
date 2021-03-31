547836d16c230214f7b0076ffe499b3314d6173a
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2021-03-31 11:33:25 +0700

    9- Custom output for LIST with pagination

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const queryString = require('query-string');
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
@@ -24,6 +25,29 @@ server.use((req, res, next) => {
   // Continue to JSON Server router
   next();
 });
+
+// Custom output for LIST with pagination
+router.render = (req, res) => {
+  // Check GET with pagination
+  // If yes, custom output
+  const headers = res.getHeaders();
+  const totalCountHeader = headers['x-total-count'];
+  if (req.method === "GET" && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader)
+      },
+    }
+    return res.jsonp(result);
+  }
+
+  // Otherwise, keep default behavior
+  res.jsonp(res.locals.data);
+}
 
 // Use default router
 server.use("/api", router);
